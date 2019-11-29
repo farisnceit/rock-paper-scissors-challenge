403e63d8f3b89f3d87d864de863a13659e92c7e0
M Mohamed Fariz <faris872010@gmail.com>
2019-11-29 08:10:04 +0530

    Working on JS Code for Game Implementation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-console.log('It Works')
+//declare Inital Variables at top
+var gameInputValues = ['paper', 'scissors', 'rock'];
+var userScore = 0;
 
-gsap.from('#header', { y: '-100%', duration: 1 })
+console.log('It Works');
+
+gsap.from('#header', { y: '-100%', duration: 1 });
+
+//target element using query Selector
+var icons = document.querySelectorAll('.icons');
+var userScoreElement = document.getElementById('user-score');
+
+//Get the value from user Clicks using dataset
+icons.forEach(icon => {
+    icon.addEventListener('click', e => {
+        var userInput = icon.dataset.value;
+        console.log(userInput);
+        console.log(gameInputValues[Math.floor(Math.random() * gameInputValues.length)]);
+
+        userScore++;
+        userScoreElement.innerHTML = userScore;
+    });
+});
