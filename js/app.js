e7640f808a046738c353e83c202cb3304c41a162
Mohamed Fariz <mohamedfariz@smackcoders.com>
2019-11-29 18:29:10 +0530

    Working Winner Section in Layout

--- js/app.js
+++ js/app.js
@@ -6,18 +6,48 @@ console.log('It Works');
 
 gsap.from('#header', { y: '-100%', duration: 1 });
 
+
+var rock = document.querySelector('.rock')
+// gsap.set(rock,{x:"-50%"});
+
+
+rock.addEventListener("mouseover", function(){gsap.set(rock,{scale:1.1});}, false);
+rock.addEventListener("mouseout", function(){gsap.set(rock,{scale:1});}, false);
+
 //target element using query Selector
 var icons = document.querySelectorAll('.icons');
 var userScoreElement = document.getElementById('user-score');
+
+var gameContainer = document.getElementById('game-container');
+var winnerBoard = document.querySelector('.winner-board');
 
 //Get the value from user Clicks using dataset
 icons.forEach(icon => {
     icon.addEventListener('click', e => {
         var userInput = icon.dataset.value;
         console.log(userInput);
-        console.log(gameInputValues[Math.floor(Math.random() * gameInputValues.length)]);
+        //console.log(gameInputValues[Math.floor(Math.random() * gameInputValues.length)]);
 
         userScore++;
         userScoreElement.innerHTML = userScore;
+
+        //Stored in Temp Variable this way to remove object reference
+        var tempInput = JSON.parse(JSON.stringify(gameInputValues))
+
+        var index = tempInput.indexOf(userInput);
+        if (index !== -1) tempInput.splice(index, 1);
+
+        console.log("filterElements",tempInput);
+        console.log('gameInputValues',gameInputValues);
+
+        //hiding other elements then Clicked for Animating
+        gsap.to(".bg-line,.icons",{scale:0,duration:1,ease: "power4.out",opacity:0});
+
+        
+        setInterval(() => {
+            gameContainer.style.display = "none";
+            winnerBoard.style.display = "block";            
+        }, 1000);
+
     });
 });
