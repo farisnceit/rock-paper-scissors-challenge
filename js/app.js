69855f85a46e8b16e38bbed9f13d4ca083fa7661
M Mohamed Fariz <faris872010@gmail.com>
2019-11-30 09:05:19 +0530

    Completed rules MOdal show and hide & Working on PLay Again btn Style

--- js/app.js
+++ js/app.js
@@ -5,14 +5,25 @@ var userScore = 0;
 console.log('It Works');
 
 gsap.from('#header', { y: '-100%', duration: 1 });
+gsap.set('.user-icon,.computer-icon', { opacity: 0, scale: 0 });
 
-
-var rock = document.querySelector('.rock')
+var rock = document.querySelector('.rock');
 // gsap.set(rock,{x:"-50%"});
 
-
-rock.addEventListener("mouseover", function(){gsap.set(rock,{scale:1.1});}, false);
-rock.addEventListener("mouseout", function(){gsap.set(rock,{scale:1});}, false);
+rock.addEventListener(
+    'mouseover',
+    function() {
+        gsap.set(rock, { scale: 1.1 });
+    },
+    false
+);
+rock.addEventListener(
+    'mouseout',
+    function() {
+        gsap.set(rock, { scale: 1 });
+    },
+    false
+);
 
 //target element using query Selector
 var icons = document.querySelectorAll('.icons');
@@ -20,6 +31,24 @@ var userScoreElement = document.getElementById('user-score');
 
 var gameContainer = document.getElementById('game-container');
 var winnerBoard = document.querySelector('.winner-board');
+
+var rulesBtn = document.querySelector('.rules-btn');
+var rulesModal = document.querySelector('.rules-modal');
+var rulesModalOverlay = document.querySelector('.rules-modal-overlay');
+var rulesModalClose = document.querySelector('.rules-modal .close');
+
+//Click EventListener for Opening Modal
+rulesBtn.addEventListener('click', params => {
+    rulesModal.classList.add('show');
+    rulesModalOverlay.classList.add('show');
+});
+
+rulesModalClose.addEventListener('click', () => {
+    rulesModal.classList.remove('show');
+    setInterval(() => {
+        rulesModalOverlay.classList.remove('show');
+    }, 500);
+});
 
 //Get the value from user Clicks using dataset
 icons.forEach(icon => {
@@ -32,22 +61,22 @@ icons.forEach(icon => {
         userScoreElement.innerHTML = userScore;
 
         //Stored in Temp Variable this way to remove object reference
-        var tempInput = JSON.parse(JSON.stringify(gameInputValues))
+        var tempInput = JSON.parse(JSON.stringify(gameInputValues));
 
         var index = tempInput.indexOf(userInput);
         if (index !== -1) tempInput.splice(index, 1);
 
-        console.log("filterElements",tempInput);
-        console.log('gameInputValues',gameInputValues);
+        console.log('filterElements', tempInput);
+        console.log('gameInputValues', gameInputValues);
 
         //hiding other elements then Clicked for Animating
-        gsap.to(".bg-line,.icons",{scale:0,duration:1,ease: "power4.out",opacity:0});
+        gsap.to('.bg-line,.icons', { scale: 0, duration: 1, ease: 'power4.out', opacity: 0 });
 
-        
         setInterval(() => {
-            gameContainer.style.display = "none";
-            winnerBoard.style.display = "block";            
+            gameContainer.style.display = 'none';
+            winnerBoard.style.display = 'flex';
         }, 1000);
 
+        gsap.to('.user-icon,.computer-icon', { opacity: 1, scale: 1, duration: 1, delay: 1, ease: 'bounce.out' });
     });
 });
